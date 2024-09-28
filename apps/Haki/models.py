e8106d1f01906ccbd660a5a312dc6e5cf761e061
Arath Duran <66040055+DuranArath150@users.noreply.github.com>
2024-09-28 14:19:40 -0700

    arreglado haki def

--- apps/Haki/models.py
+++ apps/Haki/models.py
@@ -7,5 +7,7 @@ class Haki(models.Model):
     name = models.CharField(max_length=30, blank=False, null=False)
     JPname = models.CharField(max_length=30, blank=False, null=False)
     
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
+    
+
