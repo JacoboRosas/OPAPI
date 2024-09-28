8e0a5d87fe8353bf11b5170af892ced469f1a716
Arath Duran <66040055+DuranArath150@users.noreply.github.com>
2024-09-28 13:14:39 -0700

    Bounty Model
    Se creo el modelo de bounty

--- apps/Bounty/models.py
+++ apps/Bounty/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Bounty(models.Model):
+    char_id = models.IntegerField(blank=False, null=False)
+    bounty_amount = models.BigIntegerField(blank=False, null=False)
+    
+def __str__(self):
+    return self.bounty_amount
