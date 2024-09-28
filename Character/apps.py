5fcf2f47e98c00acdfac3cacc44f2cdda2ba6635
Arath Duran <66040055+DuranArath150@users.noreply.github.com>
2024-09-28 10:55:52 -0700

    Coreegido
    asdas

--- Character/apps.py
+++ Character/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PersonajeConfig(AppConfig):
+class CharacterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Personaje'
+    name = 'Character'
