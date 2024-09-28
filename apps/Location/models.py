581ec9c28bd594f49ffc7eb51b624a2356c649fc
Arath Duran <66040055+DuranArath150@users.noreply.github.com>
2024-09-28 13:19:27 -0700

    Location added
    Added location

--- apps/Location/models.py
+++ apps/Location/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from apps.Sea.models import Sea 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    sea_id = models.ForeignKey(Sea, on_delete=models.CASCADE)
+    
+def __str__(self):
+    return self.name
+
