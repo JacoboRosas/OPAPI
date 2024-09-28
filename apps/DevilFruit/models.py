599c6bb4929371a49e0c618cfcf070a0056489d7
JacoboRosas <dangohiken@gmail..com>
2024-09-28 13:40:32 -0700

    Define Arc models.py

--- apps/DevilFruit/models.py
+++ apps/DevilFruit/models.py
@@ -6,6 +6,8 @@ class DevilFruit(models.Model):
     df_name = models.CharField(max_length=20)
     df_jp_name = models.CharField(max_lenght=20)
     category = models.CharField(max_lenght=20)
+    current_owner = models.BooleanField(default=True)
+    awakened = models.BooleanField()
 
     def __str__(self):
         return self.df_name
