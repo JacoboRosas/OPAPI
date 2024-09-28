4e0bcdf30dd3233375e329f99efb13eab6c252ae
Arath Duran <66040055+DuranArath150@users.noreply.github.com>
2024-09-28 13:38:47 -0700

    Added characters

--- apps/Character/models.py
+++ apps/Character/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from apps.Ability.models import Ability
+from apps.Arc.models import Arc
+from apps.Bounty.models import Bounty
+from apps.DevilFruit.models import DevilFruit
+from apps.Faction.models import Faction
+from apps.Haki.models import Haki
+from apps.Location.models import Location
 # Create your models here.
+
+
+CHOICES = (
+    ('Human'),
+    ('Fishmen'),
+)
+
+class Character(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    status = models.CharField(max_length=10, choices=('Alive', 'Dead', 'Unknown'), blank=False, null=False)
+    age = models.IntegerField(blank=False, null=False)
+    race = models.CharField(max_length=30, choices=CHOICES, null=False, blank=False, default='Human')
+    birthday = models.DateField()
+    height = models.DecimalField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    abilitys = models.ManyToManyField(Ability)
+    arcs = models.ManyToManyField(Arc)
+    bountys = models.ManyToManyField(Bounty)
+    devil_fruits = models.ManyToManyField(DevilFruit)
+    factions = models.ManyToManyField(Faction)
+    hakis = models.ManyToManyField(Haki)
+    
+def __str__(self):
+    return self.name
+
+    
