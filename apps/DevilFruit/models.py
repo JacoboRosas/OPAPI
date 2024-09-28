eb9271e39443d78a0db3f61b0b27aba7b5984842
JacoboRosas <dangohiken@gmail..com>
2024-09-28 12:59:51 -0700

    Define Devilfruits models.py

--- apps/DevilFruit/models.py
+++ apps/DevilFruit/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class DevilFruit(models.Model):
+    df_name = models.CharField(max_length=20)
+    df_jp_name = models.CharField(max_lenght=20)
+    category = models.CharField(max_lenght=20)
+
+    def __str__(self):
+        return self.df_name
