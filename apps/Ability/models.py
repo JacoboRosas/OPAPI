27489fddb657c5f3f1488d551484b09f0a9124e3
Arath Duran <66040055+DuranArath150@users.noreply.github.com>
2024-09-28 13:52:30 -0700

    Update models.py

--- apps/Ability/models.py
+++ apps/Ability/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Ability(models.Model):
-    ability_name = models.CharField(max_lenght=40)
-    ability_type = models.CharField(max_lenght=20)
-    description = models.CharField(max_lenght=20)
+    ability_name = models.CharField(max_length=40)
+    ability_type = models.CharField(max_length=20)
+    description = models.CharField(max_length=20)
     
     def __str__(self):
         return self.ability_name
