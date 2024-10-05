ae109a5b2eb5c7282a8018250ffd09afb5ba4486
Arath Duran <66040055+DuranArath150@users.noreply.github.com>
2024-10-05 14:05:05 -0700

    Cambios a modelo y agregar habilidades

--- apps/Ability/models.py
+++ apps/Ability/models.py
@@ -6,7 +6,7 @@ class Ability(models.Model):
     # ------- We import any attributes here (primary keys are created by default) ------- #
     ability_name = models.CharField(max_length=40)
     ability_type = models.CharField(max_length=20)
-    description = models.CharField(max_length=20)
+    description = models.CharField(max_length=250)
     
     # ------- We define here how the data entry will be shown ------- #
     def __str__(self):
