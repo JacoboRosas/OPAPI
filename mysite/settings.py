e801f14be6d32ae9899031545ec33d019414c76e
Arath Duran <66040055+DuranArath150@users.noreply.github.com>
2024-10-12 12:11:01 -0700

    Updated DB settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT':'',
     }
 }
 
